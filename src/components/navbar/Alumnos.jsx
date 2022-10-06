88ade6bd4c074765fe53c46764c65761056a34d6
mari <mari.17ymy@gmail.com>
2022-10-05 23:21:02 -0400

    instalando Auth0 y encerrando todo el App en Auth0Provider

--- src/components/navbar/Alumnos.jsx
+++ src/components/navbar/Alumnos.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import VideoPlayer from '../video/VideoPlayer'
 
 const Alumnos = () => {
   return (
     <div>
         <h1>alumnos</h1>
+        <VideoPlayer/>
     </div>
   )
 }
---
+++ src/components/video/VideoPlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+const VideoPlayer = () => {
+  return (
+    <div>
+        
+        {/* <ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/storage-proyect-b295a.appspot.com/o/files%2Fprueba2.mp4?alt=media&token=35122821-03f5-49e2-8800-1e8fe10de8c0" controls={true}/>  */}
+        <ReactPlayer url="https://firebasestorage.googleapis.com/v0/b/storage-proyect-b295a.appspot.com/o/files%2Fprueba.mp4?alt=media&token=f0ac1f70-2ea6-4ccf-a8dc-ce29c5b4efa3" controls={true}/> 
+    </div>
+  )
+}
+
+export default VideoPlayer
