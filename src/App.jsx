3782ecb026b076fb297d28b138c90173c81149cd
mari <mari.17ymy@gmail.com>
2022-10-06 01:06:40 -0400

    funcionando login y logout con auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
-
+import Profile from "./components/login/Profile";
 
 function App() {
+
   return (
     <div className="container">
       <Navbar/>
+      
+      <Profile/>
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {useAuth0} from '@auth0/auth0-react'
+
 import { BrowserRouter as Router, Routes, Route, Link, NavLink }from "react-router-dom";
 
 
@@ -6,8 +8,16 @@ import MisCursos from "./navbar/MisCursos";
 import Foro from "./navbar/Foro";
 import NuevoCurso from "./navbar/NuevoCurso";
 import Alumnos from "./navbar/Alumnos";
+import LoginButton from "./login/LoginButton";
+import LogoutButton from "./login/LogoutButton";
+
 
 const Navbar = () => {
+
+  const {isAuthenticated, isLoading} = useAuth0()
+  if(isLoading) return <h1>Loading...</h1>
+
+
   return (
     <Router>
       <div className="container mt-5">
@@ -16,6 +26,10 @@ const Navbar = () => {
           <Link to="/" className="btn btn-dark mr-2"> Mis Cursos </Link>
           <Link to="/NuevoCurso" className="btn btn-dark mr-2"> Nuevo Curso </Link>
           <NavLink to="/Foro" className="btn btn-dark mr-2"> Foro </NavLink>
+          {isAuthenticated?
+          <NavLink to="/Logout" className="btn btn-dark float-right mt-1 ml-2"> Cerrar sesion </NavLink> 
+          :<NavLink to="/Login" className="btn btn-dark float-right mt-1 ml-2"> Iniciar sesion </NavLink>
+          }
           <NavLink to="/Alumnos" className="btn btn-dark float-right mt-1"> Alumnos </NavLink>
         </div>
       <hr/>
@@ -23,6 +37,10 @@ const Navbar = () => {
           <Route path="/NuevoCurso" element={<NuevoCurso/>}/>
           <Route path="/Foro" element={<Foro/>}/> 
           <Route path="/Alumnos" element={<Alumnos/>}/> 
+
+          <Route path="/Login" element={<LoginButton/>}/> 
+          <Route path="/Logout" element={<LogoutButton/>}/> 
+
           <Route path="*" element={<MisCursos/>}/>
           
         </Routes> 
