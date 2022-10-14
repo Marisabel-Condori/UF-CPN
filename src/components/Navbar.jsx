df03b7030304fcf604ec23755dc6c17ec5724ea8
mari <mari.17ymy@gmail.com>
2022-10-14 02:29:20 -0400

    login desde cero, envia datos del login a la base de datos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ import MisCursos from "./navbar/MisCursos";
 import Foro from "./navbar/Foro";
 import NuevoCurso from "./navbar/NuevoCurso";
 import Alumnos from "./navbar/Alumnos";
-import LoginButton from "./login/LoginButton";
 import LogoutButton from "./login/LogoutButton";
+import Login from "./login/Login";
 
 
 const Navbar = () => {
@@ -28,19 +28,20 @@ const Navbar = () => {
 
   return (
     <Router>
-      <div className="container mt-5">
+      <div className="mt-2">
         <div>
         <span className="navbar-brand mb-0 h1">FCPN</span>
           <Link to="/" className="btn btn-dark mr-2"> Mis Cursos </Link>
           <Link to="/NuevoCurso" className="btn btn-dark mr-2"> Nuevo Curso </Link>
           <NavLink to="/Foro" className="btn btn-dark mr-2"> Foro </NavLink>
           {isAuthenticated? <LogoutButton/>
-          :<LoginButton/>
+          :    <Link to="/Login" className="btn btn-dark mr-2 float-right mt-1"> Login </Link>
           }
-          <NavLink to="/Alumnos" className="btn btn-dark float-right mt-1"> Alumnos </NavLink>
+          <NavLink to="/Alumnos" className="btn btn-dark float-right mt-1 mr-2"> Alumnos </NavLink>
         </div>
       <hr/>
         <Routes>
+          <Route path="/Login" element={<Login/>}/>
           <Route path="/NuevoCurso" element={<NuevoCurso/>}/>
           <Route path="/Foro" element={<Foro/>}/> 
           <Route path="/Alumnos" element={<Alumnos/>}/> 
